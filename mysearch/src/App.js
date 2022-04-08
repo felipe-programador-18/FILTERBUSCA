4fa0e193c7875a2449c149dfe9e14aeedf41b904
Felipe <machadofelipe2016@outlook.com>
2022-04-08 09:42:59 -0300

    c1-adding new code to learning and understading more about filter

--- mysearch/src/App.js
+++ mysearch/src/App.js
@@ -4,17 +4,22 @@ import './App.css';
 
 const Url = 'https://randomuser.me/api/'
 const funcionario = ['Felipe', 'Gustavo', 'Henrique','Fatima','Cristina']
+const Emplooyee = ['Programmer', 'developer','thought about my life']
+
 
 function App() {
   const [data, setdata] = useState(Url)
   const [busca, setbusca] = useState('')
-  
+  const[find, setfinds] = useState('')
+
   const filtrado = busca.toLowerCase()
   const empregadofil = funcionario
   .filter((funciona) => funciona.toLowerCase().includes(filtrado))
- 
-  const dataname = data.toLowerCase()
-  const Nome = Url.filter((name) => name.toLowerCase.includes(dataname))
+
+  //theoritical i create filter to adding data and filter too!!
+  const Union = find.toLowerCase()
+  const fiddingProff = Emplooyee.filter((fi) => fi.toLocaleLowerCase().includes(Union))
+
 
  useEffect(() =>{
    axios.get(Url)
@@ -32,14 +37,15 @@ function App() {
      ) )}
        </ul>  
 
-       <input type='text' value={data} onChange={(ev) => setdata(ev.target.value)} >
+       <input type='text' value={find} onChange={(ev)=> setfinds(ev.target.value)} >
        </input>
 
-       <ul> {Nome.map((name) => (
-         <li key={name}>{name}</li>
-       ) )}
-
+       <ul> {fiddingProff.map((fi)=> (
+          <li key={fi}> {fi} </li>
+       ))}
+        
        </ul>
+      
           
           {JSON.stringify(data)}
     </div>
